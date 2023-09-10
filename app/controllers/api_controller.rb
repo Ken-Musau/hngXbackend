9a820710fba9f2f5eb5b0ef2e1e2c5c9429083f3
Kennedy Musau <kennedy.musauu@gmail.com>
2023-09-10 19:21:08 +0300

    done

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,23 @@
+class ApiController < ApplicationController
+  def info
+    slack_name = params[:slack_name]
+    track = params[:track]
+    current_day = Time.now.strftime("%A")
+    utc_time = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")
+    github_file_url = "https://github.com/username/repo/blob/main/file_name.ext"
+    github_repo_url = "https://github.com/username/repo"
+    status_code = 200
+
+    respnse_data = {
+      slack_name: slack_name,
+      current_day: current_day,
+      utc_time: utc_time,
+      track: track,
+      github_file_url: github_file_url,
+      github_repo_url: github_repo_url,
+      status_code: status_code,
+    }
+
+    render json: respnse_data
+  end
+end
