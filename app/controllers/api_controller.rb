652b774d67b91afe48eb01d44afdfd674413dc80
Kennedy Musau <kennedy.musauu@gmail.com>
2023-09-10 19:23:23 +0300

    upate github urls

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,8 +4,8 @@ class ApiController < ApplicationController
     track = params[:track]
     current_day = Time.now.strftime("%A")
     utc_time = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")
-    github_file_url = "https://github.com/username/repo/blob/main/file_name.ext"
-    github_repo_url = "https://github.com/username/repo"
+    github_file_url = "https://github.com/Ken-Musau/hngXbackend/blob/main/app/controllers/api_controller.rb"
+    github_repo_url = "https://github.com/Ken-Musau/hngXbackend"
     status_code = 200
 
     respnse_data = {
